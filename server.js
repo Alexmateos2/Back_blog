5f5d62ba50b2efb44c358613327724431a709ebb
David <practicas2@sicosl.onmicrosoft.com>
2025-10-07 09:41:14 +0200

    coded removed

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ server.use(express.json());
 server.use(cors());
 const mysql = require('mysql2');
 const multer = require('multer');
-export {
-  DB_HOST,
-  DB_USER,
-  DB_NAME,
-  DB_PORT,
-  DB_PASSWORD,
-  
-} from './config.js'
 const connection = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
